1f72e9c3115c311ecbd41b72ab95e72c0c50f23e
mikeyb <MichaelBrown172@Gmail.com>
2018-01-13 18:59:44 -0500

    Done ================================ -Entity/Domain classes -Repo interfaces/abstacts for abstracting away and defining the behavior for interating with Domain classes -demontrative/example hard class that makes used of the repo interfaces/abstract clases Working on ================================ -Servcie interfaces/abstracts for abstracting and defining the behavior for making service calls that will in turn make use of the repos... -hard class that makes use of the interface/abstract service classes....

--- src/main/java/io/thirdplanet/reservation_monster/service_interfaces/BaseService.java
+++ src/main/java/io/thirdplanet/reservation_monster/service_interfaces/BaseService.java
@@ -1,7 +1,22 @@
 package io.thirdplanet.reservation_monster.service_interfaces;
 
+import io.thirdplanet.reservation_monster.repo_interfaces.Repository;
+
+import java.util.Collection;
+
 /**
  * Created by mikeyb on 1/13/18.
  */
-public class BaseService<TE,T> {
+public abstract class  BaseService<TE,T> extends ReadOnlyBaseService {
+    private Repository<TE,T> _repository ;
+    BaseService(Repository<TE,T> repository){
+        super(repository);
+        _repository = repository ;
+    }
+    public void add(TE entity) throws Exception{
+        _repository.add(entity);
+    }
+    public Collection<TE> getAll(){
+        return _repository.getAll();
+    }
 }
--- src/main/java/io/thirdplanet/reservation_monster/service_interfaces/ReadOnlyBaseService.java
+++ src/main/java/io/thirdplanet/reservation_monster/service_interfaces/ReadOnlyBaseService.java
@@ -1,7 +1,14 @@
 package io.thirdplanet.reservation_monster.service_interfaces;
 
+import io.thirdplanet.reservation_monster.repo_interfaces.Repository;
+
 /**
  * Created by mikeyb on 1/13/18.
  */
-public class ReadOnlyBaseService {
+public abstract class ReadOnlyBaseService<TE,T> {
+    private Repository<TE,T> repository ;
+    public ReadOnlyBaseService(Repository<TE,T> repository){
+        this.repository = repository;
+    }
+    //.............
 }
